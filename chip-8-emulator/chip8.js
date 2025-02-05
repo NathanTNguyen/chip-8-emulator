37e1d28a307e983b8055cbb7756709b903176707
NathanTNguyen <nathannguyen1590@gmail.com>
2025-02-05 21:21:44 +1100

    using tailwindcss

--- chip-8-emulator/chip8.js
+++ chip-8-emulator/chip8.js
@@ -31,7 +31,7 @@ if (ENVIRONMENT_IS_NODE) {
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: C:\Users\ALLIED~1\AppData\Local\Temp\tmp22k0jsig.js
+// include: C:\Users\ALLIED~1\AppData\Local\Temp\tmppmg4a8ez.js
 
   Module['expectedDataFileDownloads'] ??= 0;
   Module['expectedDataFileDownloads']++;
@@ -208,25 +208,25 @@ Module['FS_createPath']("/", "roms", true, true);
     }
 
     }
-    loadPackage({"files": [{"filename": "/roms/ibm-logo.ch8", "start": 0, "end": 132}], "remote_package_size": 132});
+    loadPackage({"files": [{"filename": "/roms/1-chip8-logo.ch8", "start": 0, "end": 260}, {"filename": "/roms/ibm-logo.ch8", "start": 260, "end": 392}], "remote_package_size": 392});
 
   })();
 
-// end include: C:\Users\ALLIED~1\AppData\Local\Temp\tmp22k0jsig.js
-// include: C:\Users\ALLIED~1\AppData\Local\Temp\tmpy__5fdfo.js
+// end include: C:\Users\ALLIED~1\AppData\Local\Temp\tmppmg4a8ez.js
+// include: C:\Users\ALLIED~1\AppData\Local\Temp\tmpgyv6vq_4.js
 
     // All the pre-js content up to here must remain later on, we need to run
     // it.
     if (Module['$ww'] || (typeof ENVIRONMENT_IS_PTHREAD != 'undefined' && ENVIRONMENT_IS_PTHREAD)) Module['preRun'] = [];
     var necessaryPreJSTasks = Module['preRun'].slice();
-  // end include: C:\Users\ALLIED~1\AppData\Local\Temp\tmpy__5fdfo.js
-// include: C:\Users\ALLIED~1\AppData\Local\Temp\tmp7fomzyx5.js
+  // end include: C:\Users\ALLIED~1\AppData\Local\Temp\tmpgyv6vq_4.js
+// include: C:\Users\ALLIED~1\AppData\Local\Temp\tmp9ymmttdt.js
 
     if (!Module['preRun']) throw 'Module.preRun should exist because file support used it; did a pre-js delete it?';
     necessaryPreJSTasks.forEach((task) => {
       if (Module['preRun'].indexOf(task) < 0) throw 'All preRun tasks that exist before user pre-js code should remain after; did you replace Module or modify Module.preRun?';
     });
-  // end include: C:\Users\ALLIED~1\AppData\Local\Temp\tmp7fomzyx5.js
+  // end include: C:\Users\ALLIED~1\AppData\Local\Temp\tmp9ymmttdt.js
 
 
 // Sometimes an existing Module object exists with properties
